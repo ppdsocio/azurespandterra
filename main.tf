5909c4d5f0642a5bfe66324f0893189bb7611762
Partha Datta <ppd.socio@gmail.com>
2023-05-22 17:36:41 +0530

    changing main

--- main.tf
+++ main.tf
@@ -13,22 +13,35 @@ provider "azurerm" {
   }
 }
 
-resource "azurerm_eventhub_namespace" "example" {
-  name                = "PpdTanuEventHubNamespace"
-  location            = "centralindia"
-  resource_group_name = "azurespandterra"
-  sku                 = "Standard"
-  capacity            = 1
+resource "azurerm_managed_disk" "example" {
+  name                 = "ppddiskspace"
+  location             = "centralindia"
+  resource_group_name  = "azurespandterra"
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1"
 
   tags = {
-    environment = "Production"
+    environment = "staging"
   }
 }
 
-resource "azurerm_eventhub" "example" {
-  name                = "acceptanceTestEventHub"
-  namespace_name      = azurerm_eventhub_namespace.example.name
-  resource_group_name = "azurespandterra"
-  partition_count     = 2
-  message_retention   = 1
-}
+# resource "azurerm_eventhub_namespace" "example" {
+#   name                = "PpdTanuEventHubNamespace"
+#   location            = "centralindia"
+#   resource_group_name = "azurespandterra"
+#   sku                 = "Standard"
+#   capacity            = 1
+
+#   tags = {
+#     environment = "Production"
+#   }
+# }
+
+# resource "azurerm_eventhub" "example" {
+#   name                = "acceptanceTestEventHub"
+#   namespace_name      = azurerm_eventhub_namespace.example.name
+#   resource_group_name = "azurespandterra"
+#   partition_count     = 2
+#   message_retention   = 1
+# }
